44133d4f65cf039cbba536f7862e5c9d185d2e0e
Nishad Sheriffdeen <nishadsheriffdeen@gmail.com>
2023-02-14 17:33:21 +0530

    States updated

--- my-app/src/Components/PostList.js
+++ my-app/src/Components/PostList.js
@@ -1,14 +1,28 @@
+import { useState } from "react"
+
 import classes from "./PostList.module.css"
 import Post from "./Post"
 import NewPost from "./NewPost"
 
 function Postlist(){
+
+    const [enteredBody, setEnteredBody] = useState(""); //by default empty
+    const [enteredAuthor, setEnteredAuthor] = useState("")
+
+    function changeBodyHandler(event){
+      setEnteredBody(event.target.value)
+    }
+
+    function changeAuthorHandler(event){
+        setEnteredAuthor(event.target.value)
+    }
+
     return <>
-        <NewPost/>
+        <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
         <ul className={classes.posts}>
-        <Post author = "Nishad" body = "welcome to Sri lanka"/>
-        <Post author = "Naveed" body = "welcome to Sri lanka"/>
-    </ul>
+            <Post author = {enteredAuthor} body = {enteredBody}/>
+            <Post author = "Naveed" body = "welcome to Sri lanka"/>
+        </ul>
     </>
 }
 
