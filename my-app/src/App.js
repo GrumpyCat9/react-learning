b71e8e802ddd7036cee84cb1c7a3bd63413172c8
Nishad Sheriffdeen <nishadsheriffdeen@gmail.com>
2023-02-15 11:39:14 +0530

    Header and new forms including props children added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+
 import './App.css';
 import Postlist from './Components/PostList';
+import MainHeader from './Components/MainHeader';
 
 function App() {
+
+  const [modalVisible, setModalVisibile] = useState(true);
+
+  function hideModalHandler() {
+    setModalVisibile(false);
+  }
+
+  function showModalHandler(){
+    setModalVisibile(true)
+  }
+
   return (
-    <div className="App">
-      <Postlist/>
-    </div>
+    <>
+      <MainHeader onCreatePost={showModalHandler}/>
+      <main>
+        <Postlist isPosting={modalVisible} onStopPosting={hideModalHandler}/>
+      </main>
+    </>
   );
 }
 
--- my-app/src/Components/PostList.js
+++ my-app/src/Components/PostList.js
@@ -1,29 +1,44 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import classes from "./PostList.module.css"
-import Post from "./Post"
-import NewPost from "./NewPost"
+import classes from "./PostList.module.css";
+import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
 
-function Postlist(){
+function Postlist(props) {
+  
+  const [enteredBody, setEnteredBody] = useState(""); //by default empty
+  const [enteredAuthor, setEnteredAuthor] = useState("");
 
-    const [enteredBody, setEnteredBody] = useState(""); //by default empty
-    const [enteredAuthor, setEnteredAuthor] = useState("")
+  
 
-    function changeBodyHandler(event){
-      setEnteredBody(event.target.value)
-    }
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
 
-    function changeAuthorHandler(event){
-        setEnteredAuthor(event.target.value)
-    }
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
 
-    return <>
-        <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
-        <ul className={classes.posts}>
-            <Post author = {enteredAuthor} body = {enteredBody}/>
-            <Post author = "Naveed" body = "welcome to Sri lanka"/>
-        </ul>
+
+
+  return (
+    <>
+      {props.isPosting ? (
+        <Modal onClose={props.onStopPosting}>
+          <NewPost
+            onBodyChange={changeBodyHandler}
+            onAuthorChange={changeAuthorHandler}
+          />
+        </Modal>
+      ) : false}
+
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Naveed" body="welcome to Sri lanka" />
+      </ul>
     </>
+  );
 }
 
-export default Postlist
+export default Postlist;
